feef92d9b585046accbc560dc1f4005a2ca197d6
aix <651360107@qq.com>
2023-05-24 23:53:39 +0800

    class1

--- basic/class1.cpp
+++ basic/class1.cpp
@@ -54,9 +54,105 @@ int class_A(){
 }
 
 
+///////////////////////////////////////////////////////////
+class C {
+public:
+  int a;
+  C(){
+    a1 = 1;
+    a2 = 2;
+    a3 = 3;
+    a = 4;
+  }
+  void fun(){
+    cout << a << endl;    //正确
+    cout << a1 << endl;   //正确
+    cout << a2 << endl;   //正确
+    cout << a3 << endl;   //正确
+  }
+public:
+  int a1;
+protected:
+  int a2;
+private:
+  int a3;
+};
+class D : protected C{
+public:
+  int a;
+  D(int i){
+    C();
+    a = i;
+  }
+  void fun(){
+    cout << a << endl;       //正确，public成员。
+    cout << a1 << endl;       //正确，基类的public成员，在派生类中变成了protected，可以被派生类访问。
+    cout << a2 << endl;       //正确，基类的protected成员，在派生类中还是protected，可以被派生类访问。
+    // cout << a3 << endl;       //错误，基类的private成员不能被派生类访问。
+  }
+};
+int class_C(){
+  D d(10);
+  cout << d.a << endl;       //正确。public成员
+ // cout << d.a1 << endl;      //错误，protected成员不能在类外访问。
+ // cout << d.a2 << endl;      //错误，protected成员不能在类外访问。
+ // cout << d.a3 << endl;      //错误，private成员不能在类外访问。
+ // system("pause");
+  return 0;
+}
+
+/////////////////////////
+class E{
+public:
+  int a;
+  E(){
+    a1 = 1;
+    a2 = 2;
+    a3 = 3;
+    a = 4;
+  }
+  void fun(){
+    cout << a << endl;    //正确
+    cout << a1 << endl;   //正确
+    cout << a2 << endl;   //正确
+    cout << a3 << endl;   //正确
+  }
+public:
+  int a1;
+protected:
+  int a2;
+private:
+  int a3;
+};
+class F : private A{
+public:
+  int a;
+  F(int i){
+    E();
+    a = i;
+  }
+  void fun(){
+    cout << a << endl;       //正确，public成员。
+    cout << a1 << endl;       //正确，基类public成员,在派生类中变成了private,可以被派生类访问。
+    cout << a2 << endl;       //正确，基类的protected成员，在派生类中变成了private,可以被派生类访问。
+   //  cout << a3 << endl;       //错误，基类的private成员不能被派生类访问。
+  }
+};
+int class_E(){
+  F f(10);
+  cout << f.a << endl;       //正确。public成员
+  // cout << f.a1 << endl;      //错误，private成员不能在类外访问。
+  // cout << f.a2 << endl;      //错误, private成员不能在类外访问。
+  // cout << f.a3 << endl;      //错误，private成员不能在类外访问。
+  // system("pause");
+  return 0;
+}
+
 int main()
 {
     class_A();
+    class_C();
+    class_E();
 
     return 0;
 }
