ee71bb6b2f7628f6712d245a449ed33648c47274
aix <651360107@qq.com>
2023-05-29 21:36:59 +0800

    pthread

--- basic/pthread.cpp
+++ basic/pthread.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <pthread.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -12,7 +13,7 @@ void* say_hello(void* args)
     return 0;
 }
 
-int pthread()
+int pthrea()
 {
     pthread_t tids[NUM_THREADS];
     for(int i = 0; i < NUM_THREADS; ++i)
@@ -27,9 +28,44 @@ int pthread()
     pthread_exit(NULL);
 }
 
+
+
+ 
+void *PrintHello(void *threadid)
+{  
+   // 对传入的参数进行强制类型转换，由无类型指针变为整形数指针，然后再读取
+   int tid = *((int*)threadid);
+   cout << "Hello Runoob! 线程 ID, " << tid << endl;
+   pthread_exit(NULL);
+}
+ 
+int pthrea_1 ()
+{
+   pthread_t threads[NUM_THREADS];
+   int indexes[NUM_THREADS];// 用数组来保存i的值
+   int rc;
+   int i;
+   for( i=0; i < NUM_THREADS; i++ ){      
+      cout << "main() : 创建线程, " << i << endl;
+      indexes[i] = i; //先保存i的值
+      // 传入的时候必须强制转换为void* 类型，即无类型指针        
+      rc = pthread_create(&threads[i], NULL, 
+                          PrintHello, (void *)&(indexes[i]));
+      if (rc){
+         cout << "Error:无法创建线程," << rc << endl;
+         exit(-1);
+      }
+   }
+   pthread_exit(NULL);
+}
+
+
+
 int main()
 {
-    pthread();
+    pthrea();
+    cout << endl;
+    pthrea_1();
 
     return 0;
 }
